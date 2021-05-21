91316580b7a67baa74aa3d0b174e251fc0221b0b
Vanno221 <tommaso.vanno@gmail.com>
2021-05-21 18:08:11 +0200

    Realizzazione UML

---
+++ src/PetList.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+import character.DecoratorCharacter.EquipmentCharacter;
+import observer.Subject;
+import pet.AbstractPet;
+
+public class PetList extends Subject{
+
+    private static PetList tmp = null;
+    private ArrayList<AbstractPet> pets;
+
+    private PetList(){
+        pets = new ArrayList<>();
+    }
+
+    public static PetList getObject(){
+        if (tmp == null) {
+            tmp = new PetList();
+        }
+        return tmp;
+    }
+
+    public void addCharacter(AbstractPet pet){
+        this.pets.add(pet);
+        this.notifyObservers();
+    }
+
+    public void remove(int index){
+        this.pets.remove(index);
+        this.notifyObservers();
+    }
+
+    public String toString(){
+
+        String s = null;
+
+        for(AbstractPet tmp: this.pets){
+
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            s += "RIEPILOGO PET:\n";
+            s +=  "\n";
+            s += "RAZZA: " + tmp.getRace() + "\n";
+            s += "HP: " + tmp.getHP() + "\n";
+            s += "SPEED: " + tmp.getSpeed() + "\n";
+
+        };
+        return s;
+    }
+    
+}
